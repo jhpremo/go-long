cfc1301c01495ec9c3969c7b4e1295e47ba197cc
jason <jh.premo@gmail.com>
2023-01-07 17:58:22 -0500

    created basic structure for splash page

---
+++ src/components/MainMenu/index.jsx
@@ -0,0 +1,17 @@
+import "./main-menu.css"
+
+const MainMenu = () => {
+    return (
+        <div className="main-menu-wrapper">
+            <div className="main-menu-top-half">
+                <h1 className="main-menu-title">Go Long</h1>
+                <button className="main-menu-button">Start New Game</button>
+            </div>
+            <div className="main-menu-bottom-half">
+
+            </div>
+        </div>
+    )
+}
+
+export default MainMenu
